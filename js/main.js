2d607ba83d6257283da2a4c561b5e9e10327ac14
Dan Usem <danusem@gmail.com>
2019-04-13 22:45:04 -0500

    Saturday Commit

--- js/main.js
+++ js/main.js
@@ -5,3 +5,27 @@ $('.carousel').slick({
   fade: true,
   cssEase: 'linear'
 });
+
+$('.carousel1').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+
+$('.carousel2').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+
+$('.carousel3').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
